fbb9ae71d1d3a5ef503188f4abb18afb1d645601
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-06-02 14:56:44 +0500

    256

--- client/src/utils/appConstants.js
+++ client/src/utils/appConstants.js
@@ -1,4 +1,4 @@
-export const serverUrl = "http://localhost:3000"
+// export const serverUrl = "http://localhost:3000"
 
-// export const serverUrl = "https://stripe-backend-phi.vercel.app"
+export const serverUrl = "https://chatgpt-blue-nu.vercel.app/"
 
